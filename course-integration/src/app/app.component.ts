f9f8a1aa6d4e6f2c196917d57c0adafcb442e64a
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-03 15:42:09 -0300

    create form

--- course-integration/src/app/app.component.ts
+++ course-integration/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: ``,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'course-integration';
+
+  // <h1>{{ title }}</h1>
+  //   <form [formGroup]="form" (ngSubmit)="onSubmit()">
+  //     <label>Email</label>
+  //     <input type="email" formControlName="email" />
+  //     <label>Nome</label>
+  //     <input type="text" formControlName="nome" />
+  //     <label>Senha</label>
+  //     <input type="text" formControlName="senha" />
+  //     <button type="submit">Enviar</button>
+  //   </form>
+
+  // form = new FormGroup({
+  //   email: new FormControl(''),
+  //   nome: new FormControl(''),
+  //   senha: new FormControl(''),
+  // });
+
+  // onSubmit() {
+  //   console.log(this.form.value);
+  //   const form = this.form.value;
+  //   console.log('valor: ' + form.email, form.nome, form.senha );
+  // }
 }
